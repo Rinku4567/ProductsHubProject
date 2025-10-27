7097bc04937e6301e2693a7065cb4db1c2d8cb56
Rinku Bathra <rinku45210@gmail.com>
2025-10-27 12:04:53 +0530

    Pages setup

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,27 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './Pages/Home'
+import About from './Pages/About'
+import Contact from './Pages/Contact'
+import Products from './Pages/Products'
+import Cart from './Pages/Cart'
 
 function App() {
-  
+
 
   return (
     <>
-     <h1 className='text-red-500 text-2xl'>Hello world</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />}></Route>
+          <Route path='/about' element={<About />}></Route>
+          <Route path='/contact' element={<Contact />}></Route>
+          <Route path='/Products' element={<Products />}></Route>
+          <Route path='/Cart' element={<Cart />}></Route>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
